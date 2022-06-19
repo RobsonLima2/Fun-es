ebd8dbed3340892f4497f988b305c66d70ff976d
RobsonLima2 <robson.ramos.lima@hotmail.com>
2022-06-19 17:07:27 -0300

    funções

---
+++ script.js
@@ -0,0 +1,32 @@
+//1 - crie uma função que exiba uma mensagem no console
+function msg() {
+    console.log("Minha função")
+}
+msg()
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+function nome(robson) {
+    console.log(`O meu nome é ${robson}`)
+}
+nome("Robson")
+
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+function dados(nome, idade, pagode) {
+    console.log(`O meu nome é ${nome}, minha idade é ${idade} e meu estilo musical preferido é ${pagode}`)
+    
+}
+dados("Robson","40 anos" ,"Pagode")
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+function info(vingadores, atéQueDurou) {
+    console.log(`O nome do filme é ${vingadores} e o nome da musica é ${atéQueDurou}`)
+}
+info("Os vingadores" , "Até que durou")
+
+//Bônus:
+
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+function triplo(num1) {
+    console.log(num1 = 1 * 3)
+  }
+  triplo()
